1c65316d0426f8903f9754c5fad4f6209dedfce1
BuildTools <unconfigured@null.spigotmc.org>
2017-05-27 22:44:59 -0500

    yeet

--- Extension/Loginscript.js
+++ Extension/Loginscript.js
@@ -1,11 +1,9 @@
-function onClick(){
-
-var username = localStorage.getItem(username);
-alert(localStorage[user]);
-alert(localStorage[pass]);
-var user = localStorage[user];
-var pass = localStorage[pass]
-
+function onClick() {
+    var username = localStorage.getItem(username);
+    alert(localStorage[user]);
+    alert(localStorage[pass]);
+    var user = localStorage[user];
+    var pass = localStorage[pass]
 }
 
 document.getElementById('Submit').addEventListener('click', onClick);
