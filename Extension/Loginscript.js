dda0f230fede9a9022dde64ec9f9f75d1fbd7ae7
Nathan <nathan.2002.rogers@gmail.com>
2017-05-27 22:29:55 -0500

    adam's stuff

--- Extension/Loginscript.js
+++ Extension/Loginscript.js
@@ -1,12 +1,34 @@
-function onClick(){
-
-var username = localStorage.getItem(username);
-alert(localStorage[user]);
-alert(localStorage[pass]);
-var user = localStorage[user];
-var pass = localStorage[pass]
-
+var username;
+var password;
+function onClick() {
+    getStuff();
 }
 
-document.getElementById('Submit').addEventListener('click', onClick);
+function getStuff() {
+    chrome.storage.sync.get(["username"], function(items) {
+        console.log(items.username);
+        username = items.username;
+        console.log(username);
+    });
+    chrome.storage.sync.get(["password"], function(items) {
+        console.log(items.password);
+        password = items.password;
+        console.log(password);
+    });
+    window.setTimeout(function() {
+        if (document.getElementById('username').value == username && document.getElementById('password').value == password) {
+            console.log("Yes");
+        } else {
+            console.log("No")
+        }
+    }, 3000);
+}
 
+function Auth() {
+    if (document.getElementById('username').value == username && document.getElementById('password').value == password) {
+        console.log("Yes");
+    } else {
+        console.log("No")
+    }
+}
+document.getElementById('Submit').addEventListener('click', onClick);
